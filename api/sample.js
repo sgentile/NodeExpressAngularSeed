d3f397e4ca6e6c8485197b75aa79f7b12c90f430
Steve Gentile <steven.gentile@gmail.com>
2013-11-02 09:24:22 -0400

    updated structure of sample.js and minor presentation change to main.html

--- api/sample.js
+++ api/sample.js
@@ -1,4 +1,21 @@
 function Sample(io){
+    this.get = function(req, res){
+        var data = {
+            test: "sample data"
+        }
+
+        io.sockets.emit('getcalled', {test: "sample socket data"});
+        //io.emit('an event sent to all connected clients 2');
+        res.json(data);
+    }
+
+    return {
+        get : get
+    }
+};
+module.exports = Sample;
+
+/*function Sample(io){
     Sample.get = function(req, res){
         var data = {
 			test: "sample data"
@@ -10,3 +27,4 @@ function Sample(io){
 	return Sample;
 }
 module.exports = Sample;
+}*/
